0970242367d8d6a23e2b8a7c2f79de894c1a153b
nachitoxx6262 <ignachio2003@hotmail.com>
2023-01-24 16:58:25 -0300

    modularization

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
 import { useState } from "react";
+import Square from "./components/Square"
+import Winner from "./components/Winner";
 import conffeti from "canvas-confetti"
-const TURNS = {
-  // turnos
-  X: "X",
-  O: "O",
-};
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? "is-selected" : ""}`;
-
-  const handleClick = () => {
-    updateBoard(index);
-  };
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  );
-};
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
+import {TURNS,WINNER_COMBOS} from "./constants"
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
@@ -94,19 +66,7 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
-      {winner !== null && (
-        <section className="winner">
-          <div className="text">
-            <h2>{winner == false ? "Empate" : `Ganó`}</h2>
-            <header className="win">
-              {winner && <Square>{winner}</Square>}
-            </header>
-            <footer>
-              <button onClick={resetGame}>Empezar de nuevo</button>
-            </footer>
-          </div>
-        </section>
-      )}
+      <Winner resetGame={resetGame} winner={winner}/>
     </main>
   );
 }
